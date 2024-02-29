c9005b8406b4ee3274a6d6d2d301f8648cb90b0c
leandro-ac <leandro.alvaresc@gmail.com>
2024-02-29 18:47:56 -0300

    .

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -15,15 +15,15 @@ export default function Header() {
     return(
         <header className="bg-black sticky top-0 z-20 mx-auto w-full py-8 px-4 md:p-0 mb-8">
             <div className="flex flex-wrap items-center justfy-between w-full md:w-10/12 mx:auto">
-                <Link href="/" className="flex gap-4 items-center">
+                <Link href="/" className="flex gap-4 items-center px-4">
                     <Image src={'/img/gkvaquinLogo.png'} alt={"logo"} width={904} height={904} className="h-40 w-40 rouded-xl"></Image>
                 </Link>
                 <nav className="flex justify-center">
-                    <div className="hidden w-full md:flex justify-center items-center gap-16">
+                    <div className="hidden w-full md:flex justify-end items-center gap-16">
 
                         {links.map((link, index) =>
                         <Link href={link.href} key={index}>
-                            <span className="text-2xl text-white hover:bg-white/20 p-2 rounded-xl">{link.label}</span>
+                            <span className="text-2xl text-white hover:bg-white/20 p-12 rounded-xl">{link.label}</span>
                         </Link>
                         )}
                         
