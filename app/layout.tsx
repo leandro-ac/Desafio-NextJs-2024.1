f9d742a3e9a8e1e2605f8859cffc4f8b211fa1b4
leandro-ac <leandro.alvaresc@gmail.com>
2024-03-01 16:55:27 -0300

    inicializando o footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Bebas_Neue} from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header'
+import Footer from '@/components/footer/intex'
 
 const BebasNeue = Bebas_Neue({
   weight: ['400'],
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={BebasNeue.className}>
         <Header />
         {children}
+        <Footer />
         </body>
     </html>
   )
