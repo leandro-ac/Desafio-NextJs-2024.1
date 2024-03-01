32324987f6b6d6515ca7935e64aa28bbd61f87fa
leandro-ac <leandro.alvaresc@gmail.com>
2024-03-01 17:12:59 -0300

    ajustando footer e navbar

--- components/footer/intex.tsx
+++ components/footer/intex.tsx
@@ -4,13 +4,9 @@ import Image from "next/image"
 export default function Footer(){
     return(
         <div className="bg-black w-full text-center mt-24 py-8 min-h[250px] flex flex-col gap-12 items-center justify-center">
-            <div className="px-2">
-                <span className="text-fuchsia-600">CENTRAL DE ATENDIMENTO: contato@gkvaquin.com.br</span>
-            </div>
-            <div className="px-2">    
-                <span className="text-fuchsia-600">ENDEREÇO: Rua José Lourenço Kelmer - UFJF -Instituto de Ciências Exatas - sala 3313</span>
-            </div>
-
+            <span className="text-fuchsia-600">CENTRAL DE ATENDIMENTO: contato@gkvaquin.com.br</span>  
+            <span className="text-fuchsia-600">ENDEREÇO: Rua José Lourenço Kelmer - UFJF -Instituto de Ciências Exatas - sala 3313</span>
+    
             <Image src={'/img/gkvaquinLogo.png'} alt={"logo"} width={904} height={904} className="h-40 w-60 rouded-xl"></Image>      
         </div>
     )
