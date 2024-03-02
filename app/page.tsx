8891f754e7ec646a6d1e2b510703e5871ae14a55
leandro-ac <leandro.alvaresc@gmail.com>
2024-03-02 17:16:57 -0300

    inicializando a home introduzindo a section principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { HeroSection } from '@/components/HeroSection'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-   <>
-   <h1></h1>
-   </>
+   <div>
+    <HeroSection/>
+   </div>
   )
 }
