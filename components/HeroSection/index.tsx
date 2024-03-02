5df980d3020a96cb7a58ee71e81a858c0b9fec45
leandro-ac <leandro.alvaresc@gmail.com>
2024-03-02 17:46:13 -0300

    estilizando de modo diferente a section da home

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -2,19 +2,21 @@ import Image from "next/image"
 
 export function HeroSection(){
     return(
-        <div>
-            <div>
-                <Image src="/img/instrumento.jpeg" alt={"instrumentos"} width={904} height={904} className="w-full h-[70vh]  filter grayscale"/>
+        <div className="flex items-center bg-fuchsia-600">
+            <div className="w-1/2">
+                <Image src="/img/instrumento.jpeg" alt="instrumentos" width={904} height={904} className="w-full h-[70vh] filter grayscale" />
             </div>
-            <div className="flex justify-between mt-4 bg-fuchsia-600">
-                <div className="w-1/2 p-4">
-                    <p>GKVAQUIN: O ritmo perfeito para sua melodia!</p>
+
+            <div className="w-1/2">
+                <div className="p-4">
+                    <p className="text-7xl">GKVAQUIN: O ritmo perfeito para sua melodia!</p>
                 </div>
-                <div className="w-1/2 p-4">
-                    <p>Bem-vindo ao GKvaquin!</p>
-                    <p>Desde o cavaco pro pagode até o violino pra serenata. só aqui você encontra variedade, preço e satisfação! Não perca tempo, adquira agora seu instrumento e leve a vida com mais harmonia!</p>
+                <div className="p-4">
+                    <p className="text-4xl">Bem-vindo ao GKvaquin!</p>
+                    <p className="text-2xl">Desde o cavaco pro pagode até o violino pra serenata. só aqui você encontra variedade, preço e satisfação! Não perca tempo, adquira agora seu instrumento e leve a vida com mais harmonia!</p>
                 </div>
+
             </div>
-        </div>    
+        </div>
     )
 }
